03db56f5757d25fa5bf90c8e7b43c155b8308b8c
vanshika2528 <147029479+vanshika2528@users.noreply.github.com>
2024-03-29 15:37:02 +0530

    Update db.js

--- gallery_app/backend/config/db.js
+++ gallery_app/backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectToMongo = async () => {
   const res = await mongoose.connect(
     //"mongodb://localhost:27017/gallery_app"
-    "mongodb+srv://vdhipatel77:vidhi2525@cluster0.o3cxspq.mongodb.net/"
+    "mongodb+srv://<username>:<password>@cluster0.o3cxspq.mongodb.net/"
   );
   if (res) {
     console.log("Connected Succesffuly");
